adf296158e198f4af2d50401ba62718af64327c1
AdityaKumarCos <adityakumar4644@gmail.com>
2017-10-03 12:54:09 +0530

    Added copyright and add()
    Added add() function, and a copyright notice.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b) {
+  return a+b;
+}
+
 int main() {
+  cout << "Copyright 2017 Aditya Kumar.\nSome rights reserved.\n\n\n";
   int no, nt;
   cout << "Number 1: ";
   cin >> no;
   cout << "Number 2: ";
   cin >> nt;
-  cout << "Sum: " << no + nt << ".\n";
+  cout << "Sum: " << add(no,nt) << ".\n";
 }
