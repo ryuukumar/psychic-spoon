ac0128adb47fcb9c9aa0f8c9684891b04bdb800d
AdityaKumarCos <adityakumar4644@gmail.com>
2017-10-04 11:24:45 +0530

    Added license notices
    A bit time taking, but psychic-spoon is now defended by GPL 3.0!

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+/*  
+    Copyright 2017 Aditya Kumar
+
+    This file is part of psychic-spoon.
+
+    psychic-spoon is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    psychic-spoon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with psychic-spoon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -6,7 +25,7 @@ int add(int a, int b) {
 }
 
 int main() {
-  cout << "Copyright 2017 Aditya Kumar.\nSome rights reserved.\n\n\n";
+  cout << "Copyright 2017 Aditya Kumar.\nSome rights reserved.\n\nLicensed under the GNU GPL v3.0 License\n\n\n";
   int no, nt;
   cout << "Number 1: ";
   cin >> no;
